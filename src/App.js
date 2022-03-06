936d31a3316c40513f70c1e8122399df3fc4a19c
syyad <syyadkhan@yahoo.com>
2022-03-06 11:42:59 -0500

    basic layout complete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 import "./App.css";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-// import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
+
+import {
+  Exchanges,
+  Homepage,
+  News,
+  Cryptocurrencies,
+  CryptoDetails,
+  Navbar,
+} from "./components";
 
 function App() {
   return (
     <div className="app">
-      <div className="navbar"></div>
-      <div className="main"></div>
-      <div className="footer"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/exchanges" component={Exchanges} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                component={Cryptocurrencies}
+              />
+              <Route exact path="/crypto/:coinId" component={CryptoDetails} />
+              <Route exact path="/news" component={News} />
+            </Switch>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            <Link to="/">Cryptoverse Inc.</Link> <br />
+            All Rights Reserved.
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
