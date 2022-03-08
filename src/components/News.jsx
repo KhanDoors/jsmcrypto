37003e5c3791b740e09a76f0fd19319619f378d6
Syyad Khan <syykhan@deloitte.com>
2022-03-08 12:39:22 -0500

    news cards pushed

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
 
-const News = () => {
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const demoImage =
+  "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
+
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: "Cryptocurrency",
+    count: simplified ? 6 : 12,
+  });
+
+  if (!cryptoNews?.value) return "Loading...";
+  console.log(cryptoNews);
+
   return (
-    <div>News</div>
-  )
-}
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col key={i} xs={24} sm={24} md={12} lg={8} xl={6}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+                <img
+                  src={news?.image?.thumbnail?.contentUrl || demoImage}
+                  alt={news.name}
+                />
+              </div>
+              <p>
+                {news.description > 100
+                  ? `${news.description.subString(0, 100)}...`
+                  : news.description}
+              </p>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
 
-export default News
+export default News;
