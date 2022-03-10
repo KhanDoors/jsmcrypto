40c2fbe63145d6295a94b6017f1896fdff7edfd0
Syyad Khan <syykhan@deloitte.com>
2022-03-10 09:18:42 -0500

    reversed

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -11,7 +11,8 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinPrice.push(coinHistory?.data?.history[i].price);
   }
-
+  //reverse the array so that the graph is in chronological order
+  // coinPrice.reverse();
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinTimestamp.push(
       new Date(
@@ -20,11 +21,11 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
     );
   }
   const data = {
-    labels: coinTimestamp,
+    labels: coinTimestamp.reverse(),
     datasets: [
       {
         label: "Price In USD",
-        data: coinPrice,
+        data: coinPrice.reverse(),
         fill: false,
         backgroundColor: "#0071bd",
         borderColor: "#0071bd",
